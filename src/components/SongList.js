762f3a5ae2d76f2a4fac9a5d6c6491cfc6a6fb33
Michael Duberstein <dubermic@flutterint.com>
2022-08-21 21:59:13 -0400

    In Clip 240 Building a List with Redux Data, 5:08, Select button is still above Song title

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,15 +1,28 @@
-import React, {Component} from "react";
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 class SongList extends Component {
-  render () {
-    console.log(this.props);
-    return <div>SongList</div>;
-  };
+  
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+            <div className="content">{song.title}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div>{this.renderList()}</div>;
+  }
 }
 
 // State_To_Props mapping_BEGIN
-const mapStateToProps = state => {
-  return {songs: state.songs};
+const mapStateToProps = (state) => {
+  return { songs: state.songs };
 };
 
 export default connect(mapStateToProps)(SongList);
