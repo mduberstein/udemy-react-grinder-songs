2595c204f934f308bae826496b7bb4a66abc24c2
Michael Duberstein <dubermic@flutterint.com>
2022-08-22 11:39:48 -0400

    After Clip 242 Calling Action Creators from Components

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 class SongList extends Component {
-
   renderList() {
     return this.props.songs.map((song) => {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+              // COMMON ERROR: onClick={() => selectSong(song)} 
+              // calls the action creator, but DOES NOT call store.dispatch(selectSong(song))
+              // the selectedSongReducer and thus will not update the Application State!!!
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -16,18 +24,25 @@ class SongList extends Component {
   }
 
   render() {
+    console.log("SongList's props: ", this.props);
     return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
 // State_To_Props mapping_BEGIN
 const mapStateToProps = (state) => {
+  console.log("Application State:", state);
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
 // State_To_Props mapping_END
 
-// State_To_Props mapping as defined above causes this.props ot be equal {songs: state.songs}
-// like so:
-// this.props === {songs: state.songs}
+// State_To_Props mapping, defined by the call to "connect" above, passes as a prop to the component SongList
+// 1. return value of mapStateToProps, i.e. {songs: state.songs} 
+// 2. the special wrapper for each member of the second argument, in this case "selectSong" such that
+//    this.props.selectSong(song) calls store.dispatch(selectSong(song))
+// to the props of the component
+// so in pseudolanguage the props are
+// this.props === {{songs: state.songs}, selectSong: /*store.dispatch(selectSong(...args))*/} or in syntax of ES2015
+// this.props === {{songs: state.songs}, selectSong}
